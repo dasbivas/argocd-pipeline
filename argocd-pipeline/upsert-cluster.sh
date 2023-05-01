97340a03ac9c5c498f89dda98c2e68c14bf5e454
Bivas Das <49869550+dasbivas@users.noreply.github.com>
2023-05-01 14:56:56 +0530

    Update upsert-cluster.sh

--- argocd-pipeline/upsert-cluster.sh
+++ argocd-pipeline/upsert-cluster.sh
@@ -6,7 +6,7 @@ kubectl version
 helm version
 
 # Install CRDs, ArgoCD
-MANAGED_ARGOCD_VERSION=4.6.2
+MANAGED_ARGOCD_VERSION=5.29.1
 
 echo "Install/update CRDs"
 
@@ -19,6 +19,4 @@ echo "Apply namespaces"
 helm template ../environment-charts/managed-namespaces/ | kubectl apply -f -
 
 echo "Install/update ArgoCD"
-
-helm upgrade --version $MANAGED_ARGOCD_VERSION --install argocd bitnami/argo-cd -n argocd --skip-crds 
-
+helm upgrade --version $MANAGED_ARGOCD_VERSION --install argocd ../environment-charts/managed-argocd/  -n argocd --skip-crds
